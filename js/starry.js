8e99f919d6411b4689c49988c2a88105d08216be
chris <412420984@qq.com>
2017-04-12 11:43:57 +0800

    add devbranch

---
+++ js/starry.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2017/4/12.
+ */
+var Audio_btn=document.getElementById("audio_btn");
+var Music=document.getElementById("music");
+Audio_btn.onclick=function () {
+    Music
+}
